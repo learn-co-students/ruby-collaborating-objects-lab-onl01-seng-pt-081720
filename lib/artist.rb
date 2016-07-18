fb23c1575d635beda3687de77edd0c4bc0ba0efa
Avidor Turkewitz <aviturkewitz@gmail.com>
2016-07-18 19:10:16 -0400

    sol-Fix artist and song class to work with new collaboration

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,19 +1,22 @@
 class Artist
   @@all = []
-  attr_accessor :name, :songs
+  attr_accessor :name#, :songs
 
   def initialize(name)
     @name = name
-    @songs = []
   end
 
   def self.all
     @@all    
   end
-
-  def add_song(song)
-    @songs << song
+  
+  def songs
+    Song.find_by_artist(self)
   end
+
+  # def add_song(song)
+  #   @songs << song
+  # end
 
   def self.find_or_create_by_name(name)
     self.find(name) ? self.find(name) : self.create(name)
--- lib/song.rb
+++ lib/song.rb
@@ -1,6 +1,8 @@
 class Song
   attr_accessor :name, :artist
 
+  @@all = []
+
   def initialize(name)
     @name = name
   end
@@ -9,12 +11,26 @@ class Song
     artist, song = filename.split(" - ")
     new_song = self.new(song)
     new_song.artist_name = artist
-    new_song
+    new_song.save
+  end
+
+  def self.find_by_artist(artist)
+    Song.all.select do | song |
+      song.artist == artist
+    end
+  end
+  
+  def self.all
+    @@all
   end
 
   def artist_name=(name)
     self.artist = Artist.find_or_create_by_name(name)
-    artist.add_song(self)
+  end
+  
+  def save
+    @@all << self
+    self
   end
 end
 
