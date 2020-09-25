f749057aca2362b640a1d026473e97c9a7adb719
Ernsyka Methelus <ernsykamethelus@yahoo.com>
2020-09-25 02:17:31 +0000

    Done.

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -13,7 +13,7 @@ class MP3Importer
   def import
     files.each do |file|
        s = Song.new_by_filename(file)
-       Artist.all << s.artist unless Artist.all.include?(s.artist)
+       #Artist.all << s.artist unless Artist.all.include?(s.artist)
     end
   end
   
--- lib/song.rb
+++ lib/song.rb
@@ -15,12 +15,15 @@ def self.all
   @@all
 end
 
-  def self.new_by_filename(filename)
-    split_name = filename.split (" - ")
-    song = self.new(split_name[1])
-    artist = Artist.find_or_create_by_name(split_name[0])
-    song.artist = artist
+    def artist_name=(name)
+    self.artist = Artist.find_or_create_by_name(name)
+    artist.add_song(self)
+  end
+
+  def self.new_by_filename(file)
+    song_info = file.chomp(".mp3").split(" - ")
+    song = Song.new(song_info[1])
+    song.artist_name = song_info[0]
     song
   end
-  
 end
